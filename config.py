4ee7460d79299503cce99659f0b22b1b080bf5a1
David32443 <86980889+David32443@users.noreply.github.com>
2024-01-11 22:28:11 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
